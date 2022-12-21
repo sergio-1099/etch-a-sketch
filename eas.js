7e0a9a11de4bb32d058e1ace5bd17bfbf4b0c23a
Sergio Guerra <sergioguerra6188@gmail.com>
2022-12-21 16:14:58 -0500

    Modify function to add class to new divs
    Added a uniform class so that the new divs can be styled under one class

--- eas.js
+++ eas.js
@@ -4,6 +4,7 @@ function createGrid (squares) {
     for (let i = 1; i <= numberOfDivs; i++) {
         newDiv[i] = document.createElement('div');
         newDiv[i].classList.add(`div${i}`);
+        newDiv[i].classList.add('gridSquares');
         document.querySelector('.grid-container').appendChild(newDiv[i]);
     }
 }
