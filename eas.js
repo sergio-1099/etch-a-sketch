e7d5a20641a1876003230e381bde28ed0be4d89a
Sergio Guerra <sergioguerra6188@gmail.com>
2022-12-21 16:12:20 -0500

    Modify createGrid to mulitply parameter*parameter
    This function is going to be used to make the grid. This is why it needs
    to be the parameter number times itself to create length * width.

--- eas.js
+++ eas.js
@@ -1,6 +1,7 @@
 function createGrid (squares) {
+    let numberOfDivs = squares * squares;
     let newDiv = [];
-    for (let i = 1; i <= squares; i++) {
+    for (let i = 1; i <= numberOfDivs; i++) {
         newDiv[i] = document.createElement('div');
         newDiv[i].classList.add(`div${i}`);
         document.querySelector('.grid-container').appendChild(newDiv[i]);
