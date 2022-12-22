70451aea7318c14370014a9673bdbcf73ba6f9c2
Sergio Guerra <sergioguerra6188@gmail.com>
2022-12-22 11:40:49 -0500

    Add a rainbow button that will generate random rgb
    When rainbow button is enabled, it will generate a random rgb value for
    a block that is hovered over. This allows for more user options.

--- eas.js
+++ eas.js
@@ -7,18 +7,33 @@ function createGrid (squares) {
         newDiv[i].classList.add('grid-squares');
         document.querySelector('.grid-container').appendChild(newDiv[i]);
         newDiv[i].addEventListener('mouseover', () => {
-            newDiv[i].classList.add('color-div');
+            if (rainbow == false) {
+                newDiv[i].classList.add('color-div');
+            }
+            else if (rainbow == true) {
+                newDiv[i].style.cssText += (`background-color: rgb(${randomColors()});`); 
+            }
         });
     }
     document.querySelector('.grid-container').style.cssText += (`grid-template-columns: repeat(${squares}, 1fr);`);
     document.querySelector('.grid-container').style.cssText += (`grid-template-rows: repeat(${squares}, 1fr);`);
 }
 
+function randomColors() {
+    let redValue = Math.floor(Math.random() * 256);
+    let greenValue = Math.floor(Math.random() * 256);
+    let blueValue = Math.floor(Math.random() * 256);
+    let cssString = `${redValue}, ${greenValue}, ${blueValue}`;
+    console.log(cssString);
+    return cssString;
+}
+
 let gridWidth = 16;
-createGrid(gridWidth);
+let rainbow = false;
 
 const changeGridButton = document.querySelector('.grid-size');
 const resetButton = document.querySelector('.reset');
+const rainbowButton = document.querySelector('.rainbow');
 
 changeGridButton.addEventListener('click', () => {
     let gridDivs = document.querySelectorAll('.grid-squares');
@@ -38,5 +53,18 @@ resetButton.addEventListener('click', () => {
 
     for (let i = 0; i < resetDivs.length; i++) {
         resetDivs[i].classList.remove('color-div');
+        resetDivs[i].style.cssText = '';
     }
 });
+
+rainbowButton.addEventListener('click', () => {
+    if (rainbow == true) {
+        rainbow = false;
+    }
+    else {
+        rainbow = true;
+    }
+    console.log(rainbow);
+});
+
+createGrid(gridWidth);
